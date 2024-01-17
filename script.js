1bf8efd2dace94575191b0c09f68219c657db329
Muusy <112031065+FFUV@users.noreply.github.com>
2024-01-16 21:54:12 -0500

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+const piDigits = math.pi.toString().substr(2);
+let currentIndex = 0;
+const chunkSize = 100;
+const digitsPerKeyPress = 3;
+const typewriterSound = new Howl({ src: ['typewrite.mp3'] });
+const outputElement = document.getElementById('output');
+const inputElement = document.getElementById('input');
+const terminalElement = document.getElementById('terminal');
+
+function generatePiDigits() {
+    const endIndex = currentIndex + chunkSize;
+    const currentChunk = piDigits.substring(currentIndex, endIndex);
+
+    // Update the displayed digits
+    outputElement.innerHTML += '<div class="digit-line">' + currentChunk + '</div>';
+
+    // Play typewriter sound every 100 digits
+    if (currentIndex % chunkSize === 0 && currentIndex > 0) {
+        typewriterSound.play();
+    }
+
+    currentIndex = endIndex % piDigits.length;
+
+    // Scroll to the bottom
+    outputElement.scrollTop = outputElement.scrollHeight;
+}
+
+function simulateTyping() {
+    const inputText = '> Generating Pi Digits...';
+    let index = 0;
+
+    function type() {
+        inputElement.textContent += inputText[index];
+        index++;
+
+        if (index < inputText.length) {
+            setTimeout(type, 50);
+        } else {
+            // Start generating pi digits after typing
+            setTimeout(generatePiDigits, 500);
+        }
+    }
+
+    type();
+}
+
+document.addEventListener('keydown', function (event) {
+    // Simulate typing and move down 3 digits
+    simulateTyping();
+    currentIndex += digitsPerKeyPress;
+
+    // Generate pi digits after pressing a key
+    setTimeout(generatePiDigits, 500);
+});
+
+function toggleCryptoMiner() {
+    const folderContent = document.getElementById('crypto-miner-folder').querySelector('.folder-content');
+    folderContent.classList.toggle('folder-content-visible');
+}
+
+function startCryptoMiner() {
+    const cryptoMinerButton = document.getElementById('crypto-miner-button');
+    cryptoMinerButton.classList.add('mining-animation');
+}
+
+function closeTerminal() {
+    terminalElement.style.display = 'none';
+}
+
+function minimizeTerminal() {
+    terminalElement.style.transform = 'scale(0.5)';
+}
+
+function maximizeTerminal() {
+    terminalElement.style.transform = 'scale(1)';
+}
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        const command = inputElement.textContent.trim();
+        handleCommand(command);
+        inputElement.textContent = '';
+    }
+}
+
+function handleCommand(command) {
+    // Add your custom command handling logic here
+    // For example, you can simulate executing commands and displaying output
+    outputElement.innerHTML += `<div>${command}</div>`;
+    outputElement.scrollTop = outputElement.scrollHeight;
+}
